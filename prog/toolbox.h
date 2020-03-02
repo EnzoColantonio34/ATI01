a941c7a14dab0fa2d34d2976f9e95547bf367e5f
EnzoColantonio34 <49761742+EnzoColantonio34@users.noreply.github.com>
2020-03-02 18:57:01 +0100

    Update toolbox.h

--- prog/toolbox.h
+++ prog/toolbox.h
@@ -6,3 +6,6 @@ void replace(char * message);
 
 void replace_letter(char old, char new, char * message);
 // Permet de remplacer certaines lettres par d'autres dans une chaine
+
+void upper(char * message);
+// Permet de remplacer les maj par des min (et inversement) dans une chaine
