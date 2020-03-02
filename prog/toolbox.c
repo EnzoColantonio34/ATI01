2a040b18c798729ad07f7bc28199af5db103ccbd
EnzoColantonio34 <49761742+EnzoColantonio34@users.noreply.github.com>
2020-03-02 18:55:46 +0100

    Update toolbox.c

--- prog/toolbox.c
+++ prog/toolbox.c
@@ -48,3 +48,28 @@ void replace_letter(char old, char new, char * message){
 }
 
 //////////////////////////////////////////////////////////////////////////
+// EXERCICE 4
+void upper(char * message){
+
+	int i = 0;
+	int ascii_code;
+	int convert;
+	char result;
+
+	while (message[i]!='\0'){
+		if (message[i]!=' '){
+			ascii_code=(int)message[i];
+			if (ascii_code>=65 && ascii_code<=90){
+				convert=ascii_code + 32;
+				result=(char)convert;
+				message[i]=result;
+			}
+			else if (ascii_code>=97 && ascii_code<=122){
+				convert=ascii_code - 32;
+				result=(char)convert;
+				message[i]=result;
+			}
+		}
+		i++;
+	}
+}
